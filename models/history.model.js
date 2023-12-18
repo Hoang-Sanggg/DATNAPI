8b1d67b472720bc32efc2872029fe2064314f5f9
DuyThanh00 <115547764+DThanh0@users.noreply.github.com>
2023-12-18 21:41:24 +0700

    Update history.model.js

--- models/history.model.js
+++ models/history.model.js
@@ -4,7 +4,6 @@ const HistorySchema = new mongoose.Schema(
   {
     role: { type: String, required: true },
     userId: { type: String, required: true },
-    // Các trường khác tùy thuộc vào yêu cầu của bạn
   },
   { timestamps: true }
 );
