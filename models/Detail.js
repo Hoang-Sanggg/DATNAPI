c6c87aa5387d560e286101fe9e990164a3413a3d
meouismee <136441978+meouismee@users.noreply.github.com>
2023-12-22 21:25:48 +0700

    Upload

---
+++ models/Detail.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+const detailSchema = new Schema({
+    id: { type: ObjectId },
+    Productid: { type: String, require: true },
+    gioHangid: { type: Number, require: true },
+});
+
+module.exports = mongoose.model.detail || mongoose.model('detail', detailSchema);
