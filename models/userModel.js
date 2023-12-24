2109e69eae1cc4fcdb7695cd14edd39f646fdc78
DuyThanh00 <115547764+DThanh0@users.noreply.github.com>
2023-12-24 15:50:11 +0700

    getFK_done

--- models/userModel.js
+++ models/userModel.js
@@ -4,7 +4,6 @@ const ObjectId = Schema.ObjectId;
 
 const UserSchema = new mongoose.Schema({
   id: { type: ObjectId },
-  //userId: { type: ObjectId, required: true},
   password: { type: String, required: true },
   email: { type: String, required: true },
   phone: { type: Number, required: true },
