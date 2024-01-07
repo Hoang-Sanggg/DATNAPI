e0c81d8d392910589b77a0be6ce121f27478f553
hungtqclone <121530023+hungtqclone@users.noreply.github.com>
2024-01-07 13:44:24 +0700

    update fix reactjs visit api

--- app.js
+++ app.js
@@ -19,6 +19,12 @@ const categoryDetailRouter = require('./routes/CategoryDetailApi.js');
 
 var app = express();
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
+
 require('./config/DBConnection')
 
 // view engine setup
