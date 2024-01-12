820d7456e6b76aa97d46cf279d3e89ea35910f10
hungtqclone <121530023+hungtqclone@users.noreply.github.com>
2024-01-12 12:28:29 +0700

    update fix

--- app.js
+++ app.js
@@ -6,25 +6,16 @@ var logger = require('morgan');
 
 const testRouter = require('./routes/api');
 const productRouter = require('./routes/ProductApi');
-<<<<<<< HEAD
-const DetailRouter = require('./routes/DetailAPI');
-=======
 const NotificationRouter = require('./routes/NotificationAPI');
->>>>>>> cfe3677eb396782703d8f0f3e1a3bcb4ded4d699
 const DetailsPurchaseRouter = require('./routes/DetailsPurchaseAPI');
-const NotificationRouter = require('./routes/NotificationAPI');
 const typeRouter = require('./routes/TypeApi');
 const productdetailRouter = require('./routes/ProductDetailApi');
-<<<<<<< HEAD
-const adRouter = require('./routes/AdApi');
 const chatsRouter = require('./routes/chatApi.js');
 const categoryRouter = require('./routes/CategoryApi.js');
 const categoryDetailRouter = require('./routes/CategoryDetailApi.js');
-=======
 const adRouter = require('./routes/AdApi')
 const detailCartRouter = require('./routes/detailCartAPI');
 const InforRouter = require('./routes/InforAPI');
->>>>>>> cfe3677eb396782703d8f0f3e1a3bcb4ded4d699
 
 
 var app = express();
@@ -61,15 +52,11 @@ app.use('/api/products', productRouter);
 app.use('/api/types', typeRouter);
 app.use('/api/productdetails', productdetailRouter);
 app.use('/api/ads', adRouter);
-<<<<<<< HEAD
 app.use('/api/chat', chatsRouter);
 app.use('/api/categories', categoryRouter);
 app.use('/api/categories-detail', categoryDetailRouter);
-=======
 app.use('/api/infors', InforRouter);
 
-
->>>>>>> cfe3677eb396782703d8f0f3e1a3bcb4ded4d699
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
