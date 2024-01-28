f78680b8a832978d1463f7b41f2205e9a91fda91
hungtqclone <121530023+hungtqclone@users.noreply.github.com>
2024-01-28 15:16:54 +0700

    update fix createAt

--- models/messagesModel.js
+++ models/messagesModel.js
@@ -1,13 +1,14 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const ObjectId = mongoose.ObjectId;
+const moment = require('moment-timezone');
 
 const messageSchema = new mongoose.Schema({
     id: { type: ObjectId },
     senderId: { type: ObjectId, ref: 'user' },
     receiverId: { type: ObjectId, ref: 'user' },
     content: { type: String, required: true },
-    createAt: { type: Date, default: Date.now },
+    createAt: { type: Date, default: moment.tz(new Date(), 'Asia/Ho_Chi_Minh').format() },
     seen: { type: Boolean, default: false }
 
 });
