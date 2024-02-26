d818af460a2ee583f02784ff0b8780ba777cad95
DThanh0 <115547764+DThanh0@users.noreply.github.com>
2024-02-26 23:05:01 +0700

    add filed createAT for user

--- models/userModel.js
+++ models/userModel.js
@@ -2,16 +2,18 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 
-const UserSchema = new mongoose.Schema({
-  id: { type: ObjectId },
-  password: { type: String, required: true },
-  email: { type: String, required: true },
-  phone: { type: Number, required: true },
-  name: { type: String, required: true },
-  avatar:{type: String, required: false },
-  level: { type: String, required: true, default: 0 },
-  balance: { type: Number, required:true, default:0 },
-  avatar: { type: String, required: false },
-});
+const UserSchema = new mongoose.Schema(
+  {
+    id: { type: ObjectId },
+    password: { type: String, required: true },
+    email: { type: String, required: true },
+    phone: { type: Number, required: true },
+    name: { type: String, required: true },
+    avatar: { type: String, required: false },
+    level: { type: String, required: true, default: 0 },
+    balance: { type: Number, required: true, default: 0 },
+    createAT: { type: Date, default: Date.now }
+  },
+);
 
 module.exports = mongoose.model.user || mongoose.model('user', UserSchema);
