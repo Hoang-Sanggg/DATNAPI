3be8be74faa286aca8ab710e9fd656ff1cf084a6
DThanh0 <115547764+DThanh0@users.noreply.github.com>
2024-03-28 22:55:36 +0700

    add avt defalt

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
+const avatarDefalt = 'https://static.vecteezy.com/system/resources/previews/000/439/863/original/vector-users-icon.jpg';
 
 const UserSchema = new mongoose.Schema(
   {
@@ -9,7 +10,7 @@ const UserSchema = new mongoose.Schema(
     email: { type: String, required: true },
     phone: { type: Number, required: true },
     name: { type: String, required: true },
-    avatar: { type: String, required: false },
+    avatar: { type: String, default: avatarDefalt },
     level: { type: String, required: true, default: 0 },
     balance: { type: Number, required: true, default: 0 },
     isActivate: { type: Boolean, required: false, default: true },
